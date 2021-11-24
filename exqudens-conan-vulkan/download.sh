853241fdf165693bc6798675c6e6ff5ba3b14313
ag16731 <andrii.gorin@appliedmedical.com>
2021-11-24 10:50:26 -0800

    step 8

--- exqudens-conan-vulkan/download.sh
+++ exqudens-conan-vulkan/download.sh
@@ -5,6 +5,7 @@ set -e
 rm -rf "build"
 mkdir "build"
 pushd "build"
-cp -rv "C:/VulkanSDK/1.2.182.0" "./" #"https://sdk.lunarg.com/sdk/download/1.2.189.2/windows/VulkanSDK-1.2.182.0-Installer.exe"
+curl -LJO "file:///C:/install/VulkanSDK-1.2.182.0.zip" #"https://sdk.lunarg.com/sdk/download/1.2.189.2/windows/VulkanSDK-1.2.182.0-Installer.exe"
+unzip -qq "VulkanSDK-1.2.182.0.zip"
 popd
 echo 'BUILD_SUCCESSFUL' || (echo 'BUILD_FAILED' && false)
